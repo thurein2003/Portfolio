9d13f6bc1b19e69c651a54a978abece9333a018a
nwetmonaye <nwetmon7@gamil.com>
2023-09-30 22:34:39 +0630

    nma

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -14,3 +14,40 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+  // Function to determine if an element is in the viewport
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  // Function to update active link based on the section in view
+  function updateActiveLink() {
+    navLinks.forEach((link) => {
+      const sectionId = link.getAttribute("href").substring(1);
+      const section = document.getElementById(sectionId);
+
+      if (isInViewport(section)) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  }
+
+  // Add a scroll event listener to update active link on scroll
+  window.addEventListener("scroll", updateActiveLink);
+
+  // Call the function initially to set the correct active link on page load
+  updateActiveLink();
+
+
+
+  
